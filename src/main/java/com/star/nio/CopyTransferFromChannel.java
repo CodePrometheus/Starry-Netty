f127285be574b2f2e2dadf84f42dd366de7aa2cd
CodePrometheus <Code_Prince@outlook.com>
2020-11-08 17:09:14 +0800

    BIO and NIO

---
+++ src/main/java/com/star/nio/CopyTransferFromChannel.java
@@ -0,0 +1,32 @@
+package com.star.nio;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+/**
+ * @Author: zzStar
+ * @Date: 11-08-2020 16:42
+ */
+public class CopyTransferFromChannel {
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fileInputStream = new FileInputStream("L:\\Java\\Netty\\src\\main\\java\\com\\star\\nio\\file.txt");
+        FileChannel sourceCh = fileInputStream.getChannel();
+
+        FileOutputStream fileOutputStream = new FileOutputStream("L:\\Java\\Netty\\src\\main\\java\\com\\star\\nio\\transfer.txt");
+        FileChannel destCh = fileOutputStream.getChannel();
+
+        destCh.transferFrom(sourceCh, 0, sourceCh.size());
+
+        sourceCh.close();
+        destCh.close();
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+
+
+}
---
+++ src/main/java/com/star/nio/MappedByBuffer.java
@@ -0,0 +1,11 @@
+package com.star.nio;
+
+/**
+ * 可以让文件直接在内存（堆外内存）修改，操作系统不需要拷贝一次
+ *
+ * @Author: zzStar
+ * @Date: 11-08-2020 16:54
+ */
+public class MappedByBuffer {
+
+}
